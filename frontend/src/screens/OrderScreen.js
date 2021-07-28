d0dd9c084539d40a5e56b5960b13ebf65756bd6c
Niroshan-Krishnamoorthy <niro.dev97@gmail.com>
2021-07-28 17:30:29 +0530

    checkout process initiated

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { Button, Col, Row, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
@@ -11,11 +12,15 @@ const OrderScreen = ({ match, history }) => {
 
     const orderId = match.params.id
 
+    const [sdkReady,setSdkReady] = useState(false)
+
     const dispatch = useDispatch()
 
     const orderDetails = useSelector(state => state.orderDetails)
+    const { order, loading, error } = orderDetails
 
-    const { orderItems, loading, error } = orderDetails
+    const orderPay = useSelector(state => state.orderPay)
+    const { loading:loadingPay, success:successPay, error:errorPay } = orderPay
 
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
@@ -26,12 +31,32 @@ const OrderScreen = ({ match, history }) => {
     }
 
     useEffect(() => {
+        const addPaypalScript = async() =>{
+            const {data:clientId} = await axios.get('/api/config/paypal')
+            const script = document.createElement('script')
+            script.type = 'text/javascript'
+            script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+            script.async = true
+            script.onload = ()=>{
+                setSdkReady(true)
+            }
+            document.body.appendChild(script)
+        }
+
         if (userInfo) {
-            dispatch(getOrderDetails(orderId))
+            if(successPay || !order){
+                dispatch(getOrderDetails(orderId))
+            }else if(!order.isPaid){
+                if(!window.paypal){
+                    addPaypalScript()
+                }else{
+                    setSdkReady(true)
+                }
+            }
         } else {
             history.push(`/login?redirect=order/${orderId}`)
         }
-    }, [dispatch,userInfo,orderId,history])
+    }, [dispatch,userInfo,orderId,history,successPay,order])
 
     return (
         <div>
@@ -44,16 +69,16 @@ const OrderScreen = ({ match, history }) => {
 
                                 <ListGroup.Item>
                                     <h2>Shipping</h2>
-                                    <p><strong>Name: </strong> {orderItems.user.name}</p>
-                                    <p><strong>Email: </strong> <a href={`mailto:${orderItems.user.email}`}>{orderItems.user.email}</a></p>
+                                    <p><strong>Name: </strong> {order.user.name}</p>
+                                    <p><strong>Email: </strong> <a href={`mailto:${order.user.email}`}>{order.user.email}</a></p>
                                     <p>
                                         <strong>Address: </strong>
-                                        {orderItems.shippingAddress.address},
-                                        {orderItems.shippingAddress.city},
-                                        {orderItems.shippingAddress.postalCode},
-                                        {' ' + orderItems.shippingAddress.country}
+                                        {order.shippingAddress.address},
+                                        {order.shippingAddress.city},
+                                        {order.shippingAddress.postalCode},
+                                        {' ' + order.shippingAddress.country}
                                     </p>
-                                    {orderItems.isDeleivered ? <Message varient='success'>Delivered on {orderItems.deliveredAt}</Message> :
+                                    {order.isDeleivered ? <Message varient='success'>Delivered on {order.deliveredAt}</Message> :
                                         <Message varient='danger'>Not Delivered</Message>
                                     }
                                 </ListGroup.Item>
@@ -61,17 +86,17 @@ const OrderScreen = ({ match, history }) => {
                                 <ListGroup.Item>
                                     <h2>Payment Method</h2>
                                     <p><strong>Method: </strong>
-                                        {orderItems.paymentMethod}</p>
-                                    {orderItems.isPaid ? <Message varient='success'>Paid on {orderItems.paidAt}</Message> :
+                                        {order.paymentMethod}</p>
+                                    {order.isPaid ? <Message varient='success'>Paid on {order.paidAt}</Message> :
                                         <Message varient='danger'>Not Paid</Message>
                                     }
                                 </ListGroup.Item>
 
                                 <ListGroup.Item>
                                     <h2>Order Items</h2>
-                                    {orderItems.orderItems.length === 0 ? <Message>Your order is empty</Message> : (
+                                    {order.orderItems.length === 0 ? <Message>Your order is empty</Message> : (
                                         <ListGroup variant='flush'>
-                                            {orderItems.orderItems.map((item, index) => (
+                                            {order.orderItems.map((item, index) => (
                                                 <ListGroup.Item key={index}>
                                                     <Row>
                                                         <Col md={2}>
@@ -101,25 +126,25 @@ const OrderScreen = ({ match, history }) => {
                                     <ListGroup.Item>
                                         <Row>
                                             <Col>Items</Col>
-                                            <Col>${orderItems.itemPrice}</Col>
+                                            <Col>${order.itemPrice}</Col>
                                         </Row>
                                     </ListGroup.Item>
                                     <ListGroup.Item>
                                         <Row>
                                             <Col>Shipping</Col>
-                                            <Col>${orderItems.shippingPrice}</Col>
+                                            <Col>${order.shippingPrice}</Col>
                                         </Row>
                                     </ListGroup.Item>
                                     <ListGroup.Item>
                                         <Row>
                                             <Col>Tax</Col>
-                                            <Col>${orderItems.taxPrice}</Col>
+                                            <Col>${order.taxPrice}</Col>
                                         </Row>
                                     </ListGroup.Item>
                                     <ListGroup.Item>
                                         <Row>
                                             <Col>Total</Col>
-                                            <Col>${orderItems.totalPrice}</Col>
+                                            <Col>${order.totalPrice}</Col>
                                         </Row>
                                     </ListGroup.Item>
                                     <ListGroup.Item>
